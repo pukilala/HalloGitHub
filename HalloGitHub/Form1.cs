ff934d240a41a024d77af61abe6f1d5fc22deb23
pukilala <waechter.frank@gmail.com>
2015-06-20 13:12:14 +0200

    Commit für Projektmappe ausführen

---
+++ HalloGitHub/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HalloGitHub
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i % 2 == 0)
+            {
+                richTextBox1.Text += "Hallo Git-Hub Welt";
+                i++;
+            }
+            else
+            {
+                richTextBox1.Text += " Hallo Puki Lala\n";
+                i++;
+            }
+                
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
